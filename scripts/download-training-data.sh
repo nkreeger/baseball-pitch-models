d4141dcab43235fdc670ffdb6a07bf94eeb2da3b
Nick Kreeger <nick.kreeger@gmail.com>
2018-04-06 07:09:03 -0700

    add test data download.

--- scripts/download-training-data.sh
+++ scripts/download-training-data.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+JUNE_2017_DATA="https://storage.googleapis.com/mlb-pitch-data/june_2017_pitches.csv.gz"
 JULY_2017_DATA="https://storage.googleapis.com/mlb-pitch-data/july_2017_pitches.csv.gz"
 
-target=$JULY_2017_DATA
-curl -L $target | gunzip > training_data.csv
+curl -L $JUNE_2017_DATA | gunzip > training_data.csv
+curl -L $JULY_2017_DATA | gunzip > test_data.csv
