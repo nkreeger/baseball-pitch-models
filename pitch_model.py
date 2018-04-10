b7f578214aea15f0359136054075d40ca0b09ea0
Nick Kreeger <nick.kreeger@gmail.com>
2018-04-09 20:55:56 -0700

    Drop keras model.

--- pitch_model.py
+++ pitch_model.py
@@ -7,95 +7,6 @@ import tensorflow.contrib.eager as tfe
 import pitch_data
 
 
-class Model(tf.keras.Model):
-
-  def __init__(self):
-    super(Model, self).__init__()
-
-    initializer = tf.initializers.random_uniform(0.1, 11)
-
-    self.dense1 = tf.layers.Dense(125, activation=tf.nn.relu)
-    self.dense2 = tf.layers.Dense(100, activation=tf.nn.relu)
-    self.dense3 = tf.layers.Dense(75, activation=tf.nn.relu)
-    self.dense4 = tf.layers.Dense(50, activation=tf.nn.relu)
-    self.dense5 = tf.layers.Dense(25, activation=tf.nn.relu)
-    self.dense6 = tf.layers.Dense(11, activation=None)
-    self.dropout = tf.layers.Dropout(0.5)
-
-  def __call__(self, inputs, training):
-    y = self.dense1(inputs)
-    y = self.dense2(y)
-    y = self.dense3(y)
-    y = self.dense4(y)
-    y = self.dense5(y)
-    y = self.dense6(y)
-    return y
-
-
-def compute_accuracy(logits, labels):
-  predictions = tf.argmax(logits, axis=1, output_type=tf.int64)
-  labels = tf.argmax(labels, axis=1, output_type=tf.int64)
-  batch_size = int(logits.shape[0])
-  return tf.reduce_sum(
-      tf.cast(tf.equal(predictions, labels), dtype=tf.float32)) / batch_size
-
-
-def loss(logits, labels):
-  return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels))
-
-
-def train(model, optimizer, dataset, step_counter):
-  for (batch, (labels, data)) in enumerate(tfe.Iterator(dataset)):
-    train_loop(model, optimizer, step_counter, batch, labels, data)
-
-
-def train_same_batch(model, optimizer, labels, data, step_counter):
-  for idx in range(100):
-    train_loop(model, optimizer, step_counter, idx, labels, data)
-
-
-def train_loop(model, optimizer, step_counter, batch, labels, data):
-  with tfe.GradientTape() as tape:
-    logits = model(data, training=True)
-    loss_value = loss(logits, labels)
-    accuracy = compute_accuracy(logits, labels)
-
-  grads = tape.gradient(loss_value, model.variables)
-  optimizer.apply_gradients(zip(grads, model.variables), global_step=step_counter)
-
-  if batch % 100 == 0:
-    print(' - Step #%d\tLoss: %.6f' % (batch, loss_value))
-
-
-def test(model, labels, data):
-  logits = model(data, training=False)
-  accuracy = compute_accuracy(logits, labels)
-  print(' --> Test accuracy: %.2f' % (accuracy))
-
-
-def run_eager(argv):
-  tfe.enable_eager_execution()
-
-  model = Model()
-  train_dataset = pitch_data.load_data('training_data.csv', 100)
-  test_dataset = pitch_data.load_data('test_data.csv', 100)
-  test_labels, test_data = tfe.Iterator(test_dataset).next()
-
-  step_counter = tf.train.get_or_create_global_step()
-  optimizer = tf.train.AdagradOptimizer(learning_rate=0.001)
-  # optimizer = tf.train.MomentumOptimizer(0.01, 0.5)
-
-  for _ in range(100):
-    start = time.time()
-    # TODO(kreeger): Gate this.
-    train_same_batch(model, optimizer, test_labels, test_data, step_counter)
-    # train(model, optimizer, train_dataset, step_counter)
-    end = time.time()
-    print(' ** Train time for epoch #%d (%d total steps): %f' % (_ + 1, step_counter.numpy(), end - start))
-    test(model, test_labels, test_data)
-    print('')
-
-
 def main(argv):
   col_names = pitch_data.estimator_cols()
   cols = []
@@ -106,13 +17,14 @@ def main(argv):
           feature_columns=cols,
           hidden_units=[100, 75, 50, 25],
           n_classes=11,
+          optimizer=tf.train.AdagradOptimizer(0.1),
           model_dir='models')
 
   for _ in range(1000):
     print('------ TRAIN ----------: {}'.format(_))
     classifier.train(
-            input_fn=lambda:pitch_data.csv_input_fn('training_data.csv', batchsize=25),
-            steps=2000)
+            input_fn=lambda:pitch_data.csv_input_fn('training_data.csv', batchsize=20),
+            steps=3000)
 
     print('------ EVALUATE ----------: {}'.format(_))
     eval_result = classifier.evaluate(
