30c664caf1caa2cd708133a3e741a7d09d2f8b27
Nick Kreeger <nick.kreeger@gmail.com>
2018-04-10 22:06:05 -0700

    Custom estimator.

--- pitch_model.py
+++ pitch_model.py
@@ -3,6 +3,43 @@ import tensorflow as tf
 import pitch_data
 
 
+def custom_classifier(features, labels, mode, params):
+  net = tf.feature_column.input_layer(features, params['feature_columns'])
+  for units in params['hidden_units']:
+    net = tf.layers.dense(net, units=units, activation=tf.nn.relu)
+
+    logits = tf.layers.dense(net, params['n_classes'], activation=None)
+
+    # predictions:
+    predicted_classes = tf.argmax(logits, 1)
+    if mode == tf.estimator.ModeKeys.PREDICT:
+      predictions = {
+        'class_ids': predicted_classes[:, tf.newaxis],
+        'probabilities': tf.nn.softmax(logits),
+        'logits': logits,
+      }
+      return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    accuracy = tf.metrics.accuracy(labels=labels,
+                                   predictions=predicted_classes,
+                                   name='acc_op')
+    metrics = {'accuracy': accuracy}
+    tf.summary.scalar('accuracy', accuracy[1])
+
+    if mode == tf.estimator.ModeKeys.EVAL:
+        return tf.estimator.EstimatorSpec(
+            mode, loss=loss, eval_metric_ops=metrics)
+
+    # Create training op.
+    assert mode == tf.estimator.ModeKeys.TRAIN
+
+    optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
+    train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+
+
 def model(model_dir):
   col_names = pitch_data.estimator_cols()
   cols = []
@@ -10,10 +47,10 @@ def model(model_dir):
     print('adding : {}'.format(name))
     cols.append(tf.feature_column.numeric_column(key=name))
 
-  return tf.estimator.DNNClassifier(
-          feature_columns=cols,
-          hidden_units=[250, 125, 75, 25],
-          n_classes=10,
-          optimizer=tf.train.AdamOptimizer(),
-          dropout=0.1,
-          model_dir='models')
+  return tf.estimator.Estimator(
+    model_fn=custom_classifier,
+    params={
+      'feature_columns': cols,
+      'hidden_units': [250, 125, 75, 25],
+      'n_classes': 10
+    })
--- pitch_train.py
+++ pitch_train.py
@@ -8,11 +8,11 @@ import pitch_model
 def main(argv):
   model = pitch_model.model('models')
 
-  for idx in range(1000):
+  for idx in range(10000):
     print('------ TRAIN ----------: {}'.format(idx))
     model.train(
             input_fn=lambda:pitch_data.csv_input_fn('training_data.csv', batchsize=100),
-            steps=1000)
+            steps=100)
 
     eval_result = model.evaluate(
             input_fn=lambda:pitch_data.csv_eval_fn('test_data.csv', batchsize=100))
