6541c782da09917c470de54c2a6fa72f486fff35
Nick Kreeger <nick.kreeger@gmail.com>
2018-04-12 14:34:28 -0700

    Cleanup

--- sz_model.py
+++ sz_model.py
@@ -11,6 +11,27 @@ def sz_model():
           model_dir='sz_model')
 
 
+def predict(model):
+  expected = ['STRIKE', 'BALL']
+
+  predictions = model.predict(input_fn=lambda:sz_data.predict_input(100))
+  print expected
+
+  template = ('\n** Prediction is "{}" ({:.1f}%), expected "{}"')
+  print('\n')
+  for pred_dict, expec in zip(predictions, expected):
+    class_id = pred_dict['class_ids'][0]
+    probability = pred_dict['probabilities'][class_id]
+    print(template.format(sz_data.SZ_CLASSES[class_id], 100 * probability, expec))
+
+    print('  probabilities:')
+    index = 0
+    for prob in pred_dict['probabilities']:
+      if (prob > 0):
+        print('    - {}, {}'.format((100 * prob), sz_data.SZ_CLASSES[index]))
+      index = index + 1
+
+
 def main(argv):
   model = sz_model()
 
@@ -26,18 +47,7 @@ def main(argv):
     print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
 
     if idx % 10 == 0:
-      expected = ['STRIKE', 'BALL']
-
-      predictions = model.predict(input_fn=lambda:sz_data.predict_input(100))
-      print expected
-
-      template = ('\n** Prediction is "{}" ({:.1f}%), expected "{}"')
-      print('\n')
-      for pred_dict, expec in zip(predictions, expected):
-        class_id = pred_dict['class_ids'][0]
-        probability = pred_dict['probabilities'][class_id]
-        print(template.format(sz_data.SZ_CLASSES[class_id], 100 * probability, expec))
-
+      predict(model)
 
 if __name__ == '__main__':
   tf.logging.set_verbosity(tf.logging.INFO)
