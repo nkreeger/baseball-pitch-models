eea185eb876797648d16647d21372297189faf9a
Nick Kreeger <nick.kreeger@gmail.com>
2018-04-24 17:20:38 -0700

    Updated download script.

--- scripts/download-training-data.sh
+++ scripts/download-training-data.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+# Older CSV data:
 TRAINING_DATA="https://storage.googleapis.com/mlb-pitch-data/training_data.csv.gz"
 TEST_DATA="https://storage.googleapis.com/mlb-pitch-data/test_data.csv.gz"
 
 curl -L $TRAINING_DATA | gunzip > training_data.csv
 curl -L $TEST_DATA | gunzip > test_data.csv
+
+# JSON data
+STRIKE_ZONE_TRAINING_DATA="https://storage.googleapis.com/mlb-pitch-data/strike_zone_training_data.json.gz"
+STRIKE_ZONE_TEST_DATA="https://storage.googleapis.com/mlb-pitch-data/strike_zone_test_data.json.gz"
+
+curl -L $STRIKE_ZONE_TRAINING_DATA | gunzip > strike_zone_training_data.json
+curl -L $STRIKE_ZONE_TEST_DATA | gunzip > strike_zone_test_data.json
+
